af12b5a690aec5e6d16af4a3e1b0db60e50317ab
David Jao <djao@dominia.org>
2021-04-08 11:00:11 -0400

    ssreflect migration.

--- rings.v
+++ rings.v
@@ -55,37 +55,37 @@ Section Ring_theorems.
 
   Theorem mul_0_r : ∀ a, a * 0 = 0.
   Proof.
-    intros a.
+    move=> *.
     ring.
   Qed.
 
   Theorem mul_neg_1_l : ∀ a, (-1) * a = -a.
   Proof.
-    intros a.
+    move=> *.
     ring.
   Qed.
 
   Theorem mul_neg_1_r : ∀ a, a * (-1) = -a.
   Proof.
-    intros a.
+    move=> *.
     ring.
   Qed.
 
   Theorem mul_neg_neg : ∀ a b, (-a) * (-b) = a * b.
   Proof.
-    intros a b.
+    move=> *.
     ring.
   Qed.
 
   Theorem mul_neg_l : ∀ a b, (-a) * b = - (a * b).
   Proof.
-    intros a b.
+    move=> *.
     ring.
   Qed.
 
   Theorem mul_neg_r : ∀ a b, a * (-b) = - (a * b).
   Proof.
-    intros a b.
+    move=> *.
     ring.
   Qed.
 
@@ -96,55 +96,55 @@ Section Ring_theorems.
 
   Theorem mul_0_l : ∀ a, 0 * a = 0.
   Proof.
-    intros a.
+    move=> *.
     ring.
   Qed.
 
   Theorem A3_r : ∀ a, a + 0 = a.
   Proof.
-    intros a.
+    move=> *.
     ring.
   Qed.
 
   Theorem A4_l : ∀ a, -a + a = 0.
   Proof.
-    intros a.
+    move=> *.
     ring.
   Qed.
 
   Theorem M3_r : ∀ a, a * 1 = a.
   Proof.
-    intros a.
+    move=> *.
     ring.
   Qed.
 
   Theorem D1_l : ∀ a b c, a * (b + c) = a * b + a * c.
   Proof.
-    intros a b c.
+    move=> *.
     ring.
   Qed.
 
   Theorem D1_minus_l : ∀ a b c, a * (b - c) = a * b - a * c.
   Proof.
-    intros a b c.
+    move=> *.
     ring.
   Qed.
 
   Theorem D1_minus_r : ∀ a b c, (a - b) * c = a * c - b * c.
   Proof.
-    intros a b c.
+    move=> *.
     ring.
   Qed.
 
   Theorem neg_neg : ∀ a, --a = a.
   Proof.
-    intros a.
+    move=> *.
     ring.
   Qed.
 
   Theorem difference_of_squares : ∀ a b, a * a - b * b = (a - b) * (a + b).
   Proof.
-    intros a b.
+    move=> *.
     ring.
   Qed.
 
@@ -155,21 +155,21 @@ Section Ring_theorems.
 
   Theorem div_mul_r : ∀ a b c, a｜b → a｜b*c.
   Proof.
-    intros a b c [d H].
+    move=> a b c [d H].
     exists (d*c).
     ring [H].
   Qed.
 
   Theorem div_mul_l : ∀ a b c, a｜c → a｜b*c.
   Proof.
-    intros a b c [d H].
+    move=> a b c [d H].
     exists (d*b).
     ring [H].
   Qed.
 
   Theorem div_add : ∀ a b c, a｜b → a｜c → a｜b+c.
   Proof.
-    intros a b c [x H] [y H0].
+    move=> a b c [x H] [y H0].
     exists (x+y).
     ring [H H0].
   Qed.
@@ -187,7 +187,7 @@ Section Ring_theorems.
 
   Theorem div_0_l : ∀ a, 0｜a ↔ a = 0.
   Proof.
-    split; intros H; [ destruct H as [x H] | exists 0 ]; ring [H].
+    split => [[? H] | H]; [ | exists 0 ]; ring [H].
   Qed.
 
   Theorem div_refl : ∀ a, a｜a.
@@ -199,50 +199,50 @@ Section Ring_theorems.
 
   Theorem div_trans : ∀ a b c, a｜b → b｜c → a｜c.
   Proof.
-    intros a b c [x H] [y H0].
+    move=> a b c [x H] [y H0].
     exists (x*y).
     ring [H H0].
   Qed.
 
   Theorem div_1_l : ∀ a, 1｜a.
   Proof.
-    intros a.
+    move=> a.
     exists a.
     ring.
   Qed.
 
   Theorem div_sign_r : ∀ a b, a｜b ↔ a｜-b.
   Proof.
-    split; intros [x H]; exists (-x); ring [H].
+    split => [[x H] | [x H]]; exists (-x); ring [H].
   Qed.
 
   Theorem div_sign_neg_r : ∀ a b, a｜-b → a｜b.
   Proof.
-    intros a b H.
-    now rewrite -> div_sign_r.
+    move=> *.
+    rewrite div_sign_r //.
   Qed.
 
   Theorem div_sign_r_neg : ∀ a b, a｜b → a｜-b.
   Proof.
-    intros a b H.
-    now rewrite <-div_sign_r.
+    move=> *.
+    rewrite -div_sign_r //.
   Qed.
 
   Theorem div_sign_l : ∀ a b, a｜b ↔ -a｜b.
   Proof.
-    split; intros [x H]; exists (-x); ring [H].
+    split => [[x H] | [x H]]; exists (-x); ring [H].
   Qed.
 
   Theorem div_sign_neg_l : ∀ a b, -a｜b → a｜b.
   Proof.
-    intros a b H.
-    now rewrite -> div_sign_l.
+    move=> *.
+    rewrite div_sign_l //.
   Qed.
 
   Theorem div_sign_l_neg : ∀ a b, a｜b → -a｜b.
   Proof.
-    intros a b H.
-    now rewrite <-div_sign_l.
+    move=> a b H.
+    rewrite -div_sign_l //.
   Qed.
 
   Definition assoc a b := a｜b ∧ b｜a.
@@ -256,17 +256,17 @@ Section Ring_theorems.
 
   Theorem assoc_sym : ∀ a b, a ~ b → b ~ a.
   Proof.
-    now intros a b [H H0].
+    move=> ? ? [? ?] //.
   Qed.
 
   Theorem assoc_sym_iff : ∀ a b, a ~ b ↔ b ~ a.
   Proof.
-    now split; intros [H H0].
+    split => [[] | []] //.
   Qed.
 
   Theorem assoc_trans : ∀ a b c, a ~ b → b ~ c → a ~ c.
   Proof.
-    intros a b c [H H0] [H1 H2].
+    move=> ? ? ? [? ?] [? ?].
     split; eapply div_trans; eauto.
   Qed.
 
@@ -277,75 +277,71 @@ Section Ring_theorems.
 
   Theorem assoc_zero : ∀ a, a ~ 0 ↔ a = 0.
   Proof.
-    split; intros H; subst; auto using assoc_refl.
-    destruct H; now apply div_0_l.
+    split => [[? /div_0_l] | ->]; auto using assoc_refl.
   Qed.
 
   Theorem assoc_sign : ∀ a b, a ~ b → a ~ -b.
   Proof.
-    intros a b [H H0].
+    move=> ? ? [].
     split; auto using div_sign_l_neg, div_sign_r_neg.
   Qed.
 
   Theorem unit_closure : ∀ a b, unit a → unit b → unit (a * b).
   Proof.
-    intros a b [x H] [y H0].
+    intros ? ? [x H] [y H0].
     exists (x*y).
-    rewrite <-(M3 _ 1), H, H0 at 1.
-    ring.
+    rewrite -(M3 _ 1) {1}H H0 -M2 (M2 _ a) (M1 _ a) ? M2 //.
   Qed.
 
   Theorem unit_square : ∀ u, unit (u * u) → unit u.
   Proof.
     intros u [x H].
     exists (x*u).
-    rewrite -> H.
-    ring.
+    rewrite H M2 //.
   Qed.
 
   Theorem unit_sign : ∀ a, unit a ↔ unit (-a).
   Proof.
-    split; intros H; unfold unit in *; now rewrite <-div_sign_l in *.
+    split; rewrite /unit -div_sign_l //.
   Qed.
 
   Theorem unit_sign_r : ∀ a, unit a → unit (-a).
   Proof.
-    intros a H; now apply div_sign_l_neg.
+    move=> *.
+    apply /div_sign_l_neg => //.
   Qed.
 
   Theorem one_unit : unit 1.
   Proof.
-    apply div_refl.
+    apply /div_refl.
   Qed.
 
   Theorem neg_one_unit : unit (-1).
   Proof.
-    apply unit_sign_r, one_unit.
+    apply /unit_sign_r /one_unit.
   Qed.
 
   Theorem unit_cancel : ∀ a b c, unit a → a * b = a * c → b = c.
   Proof.
-    intros a b c [x H] H0.
-    now rewrite <-(M3 _ b), H, <-M2, H0, M2, <-H, M3.
+    move=> a b c [x H] H0.
+    rewrite -(M3 _ b) H -M2 H0 M2 -H M3 //.
   Qed.
 
   Theorem cancellation_0_add : ∀ a b, a + b = 0 → b = -a.
   Proof.
-    intros a b H.
-    rewrite <-(A3 _ (-a)), <-H.
-    ring.
+    move=> a b H.
+    rewrite -(A3 _ (-a)) -H -A2 A1 -A2 A4_l A3_r //.
   Qed.
 
   Theorem cancellation_add : ∀ a b c, a + b = a + c → b = c.
   Proof.
-    intros a b c H.
-    rewrite <-(A3 _ b), <-(A4 _ a), (A1 _ a), <-A2, H.
-    ring.
+    move=> a b c H.
+    rewrite -(A3 _ b) -(A4 _ a) (A1 _ a) -A2 H A2 A4_l A3 //.
   Qed.
 
   Lemma cancellation_ne0 : ∀ a b, a * b ≠ 0 → a ≠ 0 ∧ b ≠ 0.
   Proof.
-    intros a b H; split; contradict H; subst; ring.
+    split; move: H => /[swap] ->; rewrite ? mul_0_l ? mul_0_r //.
   Qed.
 
   Definition sum f a b := iterate_with_bounds (add _) f 0 a b.
@@ -353,124 +349,105 @@ Section Ring_theorems.
 
   Theorem sum_0 : ∀ f a, sum f a a = f a.
   Proof.
-    intros f a.
-    unfold sum.
-    now rewrite -> iterate_0.
+    move=> *.
+    rewrite /sum iterate_0 //.
   Qed.
 
   Theorem sum_neg : ∀ f a b, b < a → sum f a b = 0.
   Proof.
-    intros f a b H.
-    unfold sum, iterate_with_bounds.
-    destruct excluded_middle_informative; auto.
-    now rewrite -> lt_not_ge in H.
+    move=> f a b H.
+    rewrite /sum ? iterate_neg //.
   Qed.
 
   Theorem sum_succ : ∀ f a b,
       a ≤ S b → (sum f a (S b)) = (sum f a b) + (f (S b)).
   Proof.
-    intros f a b H.
-    apply iterate_succ_lower_limit; auto.
-    ring.
+    move=> f a b H.
+    apply /iterate_succ_lower_limit; auto; ring.
   Qed.
 
   Theorem prod_0 : ∀ f a, prod f a a = f a.
   Proof.
-    intros f a.
-    unfold prod.
-    now rewrite -> iterate_0.
+    move=> *.
+    rewrite /prod iterate_0 //.
   Qed.
 
   Theorem prod_neg : ∀ f a b, b < a → prod f a b = 1.
   Proof.
-    intros f a b H.
-    unfold prod, iterate_with_bounds.
-    destruct excluded_middle_informative; auto.
-    now rewrite -> lt_not_ge in H.
+    move=> f a b H.
+    rewrite /prod ? iterate_neg //.
   Qed.
 
   Theorem prod_succ : ∀ f a b,
       a ≤ S b → (prod f a (S b)) = (prod f a b) * (f (S b)).
   Proof.
-    intros f a b H.
-    apply iterate_succ_lower_limit; auto.
-    ring.
+    move=> f a b H.
+    apply /iterate_succ_lower_limit; auto; ring.
   Qed.
 
   Theorem sum_dist :
     ∀ f g a b, sum (λ n, (f n) + (g n)) a b = sum f a b + sum g a b.
   Proof.
-    intros f g a b.
-    destruct (classic (a ≤ b)) as [[c H] | H]; subst.
-    - induction c using Induction.
-      + now rewrite -> add_0_r, ? sum_0.
-      + rewrite -> add_succ_r, ? sum_succ, IHc; try ring;
-          exists (c+1)%N; now rewrite -> add_1_r, add_succ_r.
-    - now rewrite <-lt_not_ge, ? sum_neg, A3 in *.
+    move=> f g a b.
+    elim (classic (a ≤ b)) => [[c <-] | /lt_not_ge H].
+    - induction c using Induction;
+        rewrite ? add_0_r ? sum_0 ? add_succ_r ? sum_succ ? IHc; try ring;
+          eauto using le_trans, le_add, le_succ.
+    - rewrite ? sum_neg ? A3 //.
   Qed.
 
   Theorem sum_mul : ∀ f a b c, c * sum f a b = sum (λ n, c * (f n)) a b.
   Proof.
-    intros f a b c.
-    destruct (classic (a ≤ b)) as [[d H] | H]; subst.
-    - induction d using Induction.
-      + now rewrite -> add_0_r, ? sum_0.
-      + now rewrite -> add_succ_r, ? sum_succ, D1_l, IHd;
-          try (exists (d+1)%N; now rewrite -> add_1_r, add_succ_r).
-    - now rewrite <-lt_not_ge, ? sum_neg, mul_0_r in *.
+    move=> f a b c.
+    elim (classic (a ≤ b)) => [[d <-] | /lt_not_ge H].
+    - induction d using Induction;
+        rewrite ? add_0_r ? sum_0 ? add_succ_r ? sum_succ ? D1_l ? IHd //;
+                eauto using le_trans, le_add, le_succ.
+    - rewrite ? sum_neg ? mul_0_r //.
   Qed.
 
   Theorem prod_dist :
     ∀ f g a b, prod (λ n, (f n) * (g n)) a b = prod f a b * prod g a b.
   Proof.
-    intros f g a b.
-    destruct (classic (a ≤ b)) as [[c H] | H]; subst.
-    - induction c using Induction.
-      + now rewrite -> add_0_r, ? prod_0.
-      + rewrite -> add_succ_r, ? prod_succ, IHc; try ring;
-          exists (c+1)%N; now rewrite -> add_1_r, add_succ_r.
-    - now rewrite <-lt_not_ge, ? prod_neg, M3 in *.
+    move=> f g a b.
+    elim (classic (a ≤ b)) => [[c <-] | /lt_not_ge H].
+    - induction c using Induction;
+        rewrite ? add_0_r ? prod_0 ? add_succ_r ? prod_succ ? IHc; try ring;
+          eauto using le_trans, le_add, le_succ.
+    - rewrite ? prod_neg ? M3 //.
   Qed.
 
   Theorem sum_of_0 : ∀ d, (sum (λ n, 0) 0 d) = 0.
   Proof.
-    induction d using Induction.
-    - apply @iterate_0.
-    - rewrite -> sum_succ, IHd; auto using zero_le; ring.
+    induction d using Induction;
+      rewrite ? sum_0 ? sum_succ ? IHd; auto using zero_le; ring.
   Qed.
 
   Theorem prod_of_1 : ∀ d, (prod (λ n, 1) 0 d) = 1.
   Proof.
-    induction d using Induction.
-    - apply @iterate_0.
-    - rewrite -> prod_succ, IHd; auto using zero_le; ring.
+    induction d using Induction;
+      rewrite ? prod_0 ? prod_succ ? IHd; auto using zero_le; ring.
   Qed.
 
   Theorem unit_prod_closure_0 :
     ∀ n f, (∀ i, 0 ≤ i ≤ n → unit (f i)) → unit (prod f 0 n).
   Proof.
-    induction n using Induction; intros f H.
-    - rewrite -> prod_0.
-      auto using le_refl.
-    - rewrite prod_succ; try apply unit_closure; eauto using le_refl, zero_le.
-      apply IHn.
-      intros i [H0 H1].
-      eauto using le_trans, le_succ.
+    induction n using Induction =>
+    f H; rewrite ? prod_0 ? prod_succ; try apply unit_closure; try apply IHn;
+      intuition eauto using le_refl, zero_le, le_trans, le_succ.
   Qed.
 
   Theorem unit_prod_closure :
     ∀ a b f, (∀ i, a ≤ i ≤ b → unit (f i)) → unit (prod f a b).
   Proof.
-    intros a b f H.
-    destruct (classic (a ≤ b)%N) as [[c H0] | H0]; subst.
-    - unfold prod.
-      rewrite -> iterate_shift.
-      apply unit_prod_closure_0.
-      intros i H0.
+    move=> a b f.
+    elim (classic (a ≤ b)%N) => [[c <-] H | /lt_not_ge H0].
+    - rewrite /prod iterate_shift.
+      apply /unit_prod_closure_0 => i H0.
       apply H.
-      rewrite <-(add_0_l a), ? (add_comm a) at 1.
-      split; apply O1_le; intuition.
-    - rewrite <-lt_not_ge, ? prod_neg in *; auto using one_unit.
+      rewrite -{1}(add_0_l a) (add_comm a).
+      intuition eauto using O1_le.
+    - rewrite prod_neg; auto using one_unit.
   Qed.
 
   Definition pow a n := prod (λ x, a) 1 n.
@@ -479,87 +456,75 @@ Section Ring_theorems.
 
   Theorem pow_0_r : ∀ x, x^0 = 1.
   Proof.
-    intros x.
-    unfold pow.
-    rewrite -> prod_neg; now try apply lt_succ.
+    move=> *.
+    rewrite /pow prod_neg; eauto using lt_succ.
   Qed.
 
   Theorem pow_succ_r : ∀ x y, x^(S y) = x^y * x.
   Proof.
-    intros x y.
-    unfold pow.
-    rewrite -> prod_succ; auto using one_le_succ.
+    move=> *.
+    rewrite /pow prod_succ; auto using one_le_succ.
   Qed.
 
   Theorem pow_1_r : ∀ x, x^1 = x.
   Proof.
-    intros x.
-    unfold naturals.one.
-    now rewrite -> pow_succ_r, pow_0_r, M3.
+    move=> *.
+    rewrite /naturals.one pow_succ_r pow_0_r M3 //.
   Qed.
 
   Theorem pow_1_l : ∀ x, 1^x = 1.
   Proof.
-    induction x using Induction.
-    - now rewrite -> pow_0_r.
-    - now rewrite -> pow_succ_r, IHx, M3.
+    induction x using Induction; rewrite ? pow_0_r ? pow_succ_r ? IHx ? M3 //.
   Qed.
 
   Theorem pow_0_l : ∀ x, x ≠ 0%N → 0^x = 0.
   Proof.
-    induction x using Induction; intros H; try tauto.
-    now rewrite -> pow_succ_r, mul_0_r.
+    induction x using Induction => H //.
+    rewrite pow_succ_r mul_0_r //.
   Qed.
 
   Theorem pow_2_r : ∀ x, x^2 = x * x.
   Proof.
-    intros x.
-    now rewrite -> pow_succ_r, pow_1_r.
+    move=> *.
+    rewrite pow_succ_r pow_1_r //.
   Qed.
 
   Theorem pow_add_r : ∀ a b c, a^(b+c) = a^b * a^c.
   Proof.
-    induction c using Induction.
-    - now rewrite -> add_0_r, pow_0_r, M3_r.
-    - now rewrite -> add_succ_r, ? pow_succ_r, IHc, M2.
+    induction c using Induction;
+      rewrite ? add_0_r ? pow_0_r ? M3_r ? add_succ_r ? pow_succ_r ? IHc ? M2//.
   Qed.
 
   Theorem pow_mul_l : ∀ a b c, (a*b)^c = a^c * b^c.
   Proof.
-    induction c using Induction.
-    - now rewrite -> ? pow_0_r, M3.
-    - now rewrite -> ? pow_succ_r, <-? M2, (M2 _ a), (M1 _ _ (b^c)), IHc, ? M2.
+    induction c using Induction;
+      rewrite ? pow_0_r ? M3 ? pow_succ_r -? M2 ? (M2 _ a)
+              ? (M1 _ _ (b^c)) ? IHc ? M2 //.
   Qed.
 
   Theorem pow_mul_r : ∀ a b c, a^(b*c) = (a^b)^c.
   Proof.
-    induction c using Induction.
-    - now rewrite -> naturals.mul_0_r, ? pow_0_r.
-    - now rewrite -> mul_succ_r, pow_succ_r, pow_add_r, IHc.
+    induction c using Induction;
+      rewrite ? naturals.mul_0_r ? pow_0_r ? mul_succ_r
+              ? pow_succ_r ? pow_add_r ? IHc //.
   Qed.
 
   Theorem prod_mul : ∀ f a b c,
       a ≤ b → c^(S (b-a)) * prod f a b = prod (λ n, c * (f n)) a b.
   Proof.
-    intros f a b c [d H].
-    subst.
-    induction d using Induction.
-    - now rewrite -> add_0_r, sub_diag, pow_1_r, ? prod_0.
-    - rewrite -> ? (add_comm a), sub_abba, ? pow_succ_r, ? (add_comm _ a),
-      add_succ_r, ? prod_succ, (add_comm a), <-IHd in *;
-        try (exists (d+1)%N; rewrite <-? add_1_r); ring.
+    intros f a b c [d <-].
+    elim/Induction: d => [ | n].
+    - rewrite add_0_r sub_diag pow_1_r ? prod_0 //.
+    - rewrite ? (add_comm a) ? sub_abba ? pow_succ_r add_succ_l ? prod_succ
+              // => [ | | <-]; eauto using le_trans, le_add_l, le_succ; ring.
   Qed.
 
   Theorem unit_pow_closure : ∀ a n, 0 < n → unit (a^n) → unit a.
   Proof.
-    intros a n H H0.
-    destruct (classic (n = 0%N)) as [H1 | H1];
-      try apply succ_0 in H1 as [m H1]; subst.
-    - contradiction (lt_irrefl 0).
-    - rewrite -> pow_succ_r in H0.
-      destruct H0 as [x H0].
-      exists (x * a^m).
-      now rewrite -> H0, M2.
+    move=> a n.
+    elim (classic (n = 0%N)) => [-> /lt_irrefl | /succ_0 [m ->] H] //.
+    rewrite pow_succ_r /unit /divide => [[x]].
+    eauto using M2, eq_trans, eq_sym.
   Qed.
 
   Definition INR (n : N) := sum (λ n, 1) 1 n.
@@ -567,41 +532,28 @@ Section Ring_theorems.
 
   Theorem INR_zero : 0 = 0%N.
   Proof.
-    unfold INR.
-    rewrite -> sum_neg; eauto using naturals.succ_lt.
+    rewrite /INR sum_neg; eauto using naturals.succ_lt.
   Qed.
 
   Theorem INR_one : 1 = 1%N.
   Proof.
-    unfold INR.
-    now rewrite -> sum_0.
+    rewrite /INR sum_0 //.
   Qed.
 
   Theorem INR_add : ∀ a b : N, a + b = (a + b)%N.
   Proof.
-    intros a b.
-    unfold INR.
-    induction b using Induction.
-    { fold (INR 0).
-      now rewrite <-INR_zero, add_0_r, A1, A3. }
-    rewrite -> add_succ_r, ? sum_succ, <-IHb; try ring;
-      [ exists (a+b)%N | exists b ]; now rewrite <-add_1_r, naturals.add_comm.
+    move=> a.
+    (elim/Induction; try rewrite -INR_zero add_0_r A1 A3 //) => b H.
+    rewrite /INR add_succ_r ? sum_succ -?/(INR (a+b)%N) -? H ? A2 //;
+            [exists b | exists (a+b)%N]; rewrite -add_1_r naturals.add_comm //.
   Qed.
 
   Theorem INR_mul : ∀ a b : N, a * b = ((a * b)%N).
   Proof.
-    intros a b.
-    unfold INR.
-    induction b using Induction.
-    { rewrite -> naturals.mul_0_r.
-      fold (INR 0).
-      now rewrite <-? INR_zero, mul_0_r. }
-    rewrite -> mul_succ_r, sum_succ, D1_l, IHb.
-    - fold (INR (a*b)) (INR a) (INR (a*b+a)).
-      ring_simplify.
-      apply INR_add.
-    - exists b.
-      now rewrite <-add_1_r, naturals.add_comm.
+    move=> a.
+    (elim/Induction; try rewrite naturals.mul_0_r -? INR_zero mul_0_r //) => b.
+    rewrite /INR mul_succ_r sum_succ ? D1_l ? M3_r -/(INR (a*b+a))
+            -? INR_add => [ | ->]; auto using one_le_succ.
   Qed.
 
   Section Subring_construction.
