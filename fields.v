c2def036aa5b7ec05474c6a31d5cfd101a45abd1
David Jao <djao@dominia.org>
2021-04-25 15:44:46 -0400

    ssreflect migration (fields.v completed).

--- fields.v
+++ fields.v
@@ -50,17 +50,16 @@ Section Field_theorems.
 
   Theorem inv_r : ∀ a, a ≠ 0 → a * a^-1 = 1.
   Proof.
-    intros a H.
-    now field.
+    move=> a H.
+      by field.
   Qed.
   Definition M4_r := inv_r.
 
   Theorem cancellation : ∀ a b, a * b = 0 → a = 0 ∨ b = 0.
   Proof.
-    intros a b H.
-    destruct (classic (a = 0)) as [H0 | H0]; try tauto.
-    apply (f_equal (rings.mul _ (a^-1))) in H.
-    rewrite -> rings.M2, M4, rings.M3, rings.mul_0_r in H; auto.
+    move: classic => /[swap] a /(_ (a = 0)) [/or_introl | H]
+                      b /(f_equal (rings.mul _ (a^-1))) //.
+    rewrite rings.M2 M4 // rings.M3 mul_0_r => /or_intror //.
   Qed.
 
   Definition integral_domain_from_field :=
@@ -69,21 +68,19 @@ Section Field_theorems.
   Theorem inv_one : 1^-1 = 1.
   Proof.
     field.
-    apply one_ne_0.
+    apply /one_ne_0.
   Qed.
 
   Theorem inv_unique : ∀ a, (∀ b, a * b = 1 → b = a^-1).
   Proof.
-    intros a b H.
-    destruct (classic (a = 0)) as [H0 | H0]; subst.
-    - contradiction (one_ne_0 Field).
-      now ring_simplify in H.
-    - now field [H].
+    move: classic => /[swap] a /(_ (a = 0)) [-> b | H b H0].
+    - move: mul_0_l (one_ne_0 Field) -> => /neq_sym //.
+    - by field [H0].
   Qed.
 
   Theorem inv_neg : ∀ a, a ≠ 0 → -a^-1 = (-a)^-1.
   Proof.
-    intros a H.
+    move=> a H.
     field.
     split; auto.
     contradict H.
@@ -92,9 +89,8 @@ Section Field_theorems.
 
   Theorem inv_ne_0 : ∀ a, a ≠ 0 → a^-1 ≠ 0.
   Proof.
-    intros a H H0.
-    contradiction (one_ne_0 Field).
-    now rewrite <-(inv_r a), H0, mul_0_r.
+    move: (one_ne_0 Field) => /[swap] a /[swap] H0 /[swap] H1 [].
+    rewrite -(inv_r a) // H1 mul_0_r //.
   Qed.
 
   Theorem inv_inv : ∀ a, a ≠ 0 → a^-1^-1 = a.
@@ -104,20 +100,17 @@ Section Field_theorems.
 
   Theorem unit_nonzero : ∀ a, rings.unit a ↔ a ≠ 0.
   Proof.
-    split.
-    - intros [x H] H0.
-      subst.
-      rewrite mul_0_r in H.
-      contradiction (one_ne_0 Field).
+    split => [/[swap] -> [x] | ].
+    - move: mul_0_r (one_ne_0 Field) -> => //.
     - exists (a^-1).
-      now rewrite inv_l.
+      rewrite inv_l //.
   Qed.
 
   Theorem inv_ring_to_field : ∀ a, a ≠ 0 → integer_powers.inv _ a = a^-1.
   Proof.
-    intros a H.
-    apply inv_unique.
-    now rewrite integer_powers.inv_r; try apply unit_nonzero.
+    move=> a /unit_nonzero H.
+    apply /inv_unique.
+    rewrite integer_powers.inv_r //.
   Qed.
 
   Definition pow_N := (rings.pow (ring Field)) : F → N → F.
@@ -133,100 +126,92 @@ Section Field_theorems.
 
   Theorem pow_0_r : ∀ a, a^0 = 1.
   Proof.
-    intros a.
-    apply pow_0_r.
+    move=> a.
+      apply /pow_0_r.
   Qed.
 
   Theorem pow_0_l : ∀ a : Z, a ≠ 0%Z → 0^a = 0.
   Proof.
-    intros a H.
-    now apply pow_0_l.
+    move=> a H.
+      by apply /pow_0_l.
   Qed.
 
   Theorem pow_1_r : ∀ a, a^1 = a.
   Proof.
-    intros a.
-    apply pow_1_r.
+    move=> a.
+    apply /pow_1_r.
   Qed.
 
   Theorem pow_1_l : ∀ a, 1^a = 1.
   Proof.
-    intros a.
-    apply pow_1_l.
+    move=> a.
+    apply /pow_1_l.
   Qed.
 
   Theorem pow_neg : ∀ a b, a ≠ 0 → a^(-b) = (a^-1)^b.
   Proof.
-    intros a b H.
-    apply unit_nonzero in H as H0.
-    rewrite -> (pow_neg (ring Field)); auto.
-    unfold pow, ring_from_field; simpl.
-    f_equal.
-    now apply inv_ring_to_field.
+    move=> a b /[dup] H /unit_nonzero H0.
+    rewrite /pow pow_neg /ring_from_field // inv_ring_to_field //.
   Qed.
 
   Theorem inv_pow : ∀ a, a ≠ 0 → a^(-1) = a^-1.
   Proof.
-    intros a H.
-    now rewrite -> pow_neg, pow_1_r.
+    move=> a H.
+    rewrite pow_neg // pow_1_r //.
   Qed.
 
   Theorem pow_add_r : ∀ a b c, a ≠ 0 → a^(b+c) = a^b * a^c.
   Proof.
-    intros a b c H.
-    now apply pow_add_r, unit_nonzero.
+    move=> a b c /unit_nonzero /pow_add_r //.
   Qed.
 
   Theorem pow_ne_0 : ∀ a b, a ≠ 0 → a^b ≠ 0.
   Proof.
-    intros a b H.
-    now apply unit_nonzero, unit_pow, unit_nonzero.
+    move=> a b H.
+      by apply /unit_nonzero /unit_pow /unit_nonzero.
   Qed.
 
   Theorem pow_mul_l : ∀ a b c, a ≠ 0 → b ≠ 0 → (a*b)^c = a^c * b^c.
   Proof.
-    intros a b c H H0.
-    apply pow_mul_l; now apply unit_nonzero.
+    move=> a b c /unit_nonzero H /unit_nonzero H0.
+      by apply /pow_mul_l.
   Qed.
 
   Theorem neg_pow : ∀ a b, a ≠ 0 → a^(-b) = (a^b)^-1.
   Proof.
-    intros a b H.
-    apply unit_nonzero in H as H0.
-    rewrite -> (neg_pow (ring Field)); auto.
-    now apply inv_ring_to_field, pow_ne_0.
+    move=> a b /[dup] H /unit_nonzero H0.
+    rewrite /pow (neg_pow (ring Field)) //.
+      by apply /inv_ring_to_field /pow_ne_0.
   Qed.
 
   Theorem pow_mul_r : ∀ a b c, a ≠ 0 → a^(b*c) = (a^b)^c.
   Proof.
-    intros a b c H.
-    now apply pow_mul_r, unit_nonzero.
+    move=> a b c H.
+      by apply /pow_mul_r /unit_nonzero.
   Qed.
 
   Theorem pow_div_distr : ∀ a b c, a ≠ 0 → b ≠ 0 → (a*b^-1)^c = a^c * (b^c)^-1.
   Proof.
-    intros a b c H H0.
-    rewrite -> pow_mul_l, <-neg_pow, pow_neg; auto using inv_ne_0.
+    move=> a b c H /[dup] H0 /inv_ne_0 H1.
+    rewrite pow_mul_l // -neg_pow // pow_neg //.
   Qed.
 
   Lemma pow_add_r_opp : ∀ a b, a ≠ 0 → a^b * a^(-b) = 1.
   Proof.
-    intros a b H.
-    now apply pow_add_r_opp, unit_nonzero.
+    move=> a b H.
+      by apply /pow_add_r_opp /unit_nonzero.
   Qed.
 
   Theorem pow_wf : ∀ a b, a**b = a^b.
   Proof.
-    intros a b.
-    apply eq_sym, pow_nonneg.
+    move=> a b.
+      by apply /eq_sym /pow_nonneg.
   Qed.
 
   Theorem minus_one_nonzero : -1 ≠ 0.
   Proof.
-    intros H.
-    apply (f_equal (rings.neg (ring Field))) in H.
-    ring_simplify in H.
-    contradiction (one_ne_0 Field).
+    move: (one_ne_0 Field) =>/[swap] /(f_equal (rings.neg (ring Field))) H [].
+    ring_simplify in H => //.
   Qed.
 
 End Field_theorems.
