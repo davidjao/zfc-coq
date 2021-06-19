dd270d3c0ea4535659e35fd712786accd5e390c8
David Jao <djao@dominia.org>
2021-06-18 20:52:13 -0400

    Constructive proofs of Russell's paradox.

--- russell.v
+++ russell.v
@@ -10,25 +10,23 @@ Section Russell's_paradox.
   (* Proof of False from Frege's universal comprehension axiom. *)
   Theorem UC_False : False.
   Proof.
-    elim (Universal_comprehension (λ x, x ∉ x)) => [x /(_ x)] // [H H0].
-    elim (classic (x ∈ x)) => [/[dup] /H | /[dup] /H0] //.
+    elim (Universal_comprehension (λ x, x ∉ x)) => [x /(_ x)].
+    tauto.
   Qed.
 
   (* Proof of False from universal set axiom. *)
   Theorem US_False : False.
   Proof.
     move: Universal_set => [X H].
-    elim (classic ({x in X | x ∉ x} ∈ {x in X | x ∉ x})) =>
-    [/[dup] H0 /Specify_classification [H1 H2] | /[dup] H0 H1] //.
-    apply /H1 /Specify_classification => //.
+    have: {x in X | x ∉ x} ∉ {x in X | x ∉ x} =>
+    /[dup] H0 /Specify_classification [] //.
   Qed.
 
   (* Proof that universal comprehension implies universal set. *)
   Theorem UC_implies_US : ∃ x, ∀ y, y ∈ x.
   Proof.
     move: (Universal_comprehension (λ x, ∀ y : set, y = y)) => [x H].
-    exists x => y.
-    apply /H => //.
+    firstorder.
   Qed.
 
   (* Proof that universal set implies universal comprehension. *)
@@ -37,7 +35,7 @@ Section Russell's_paradox.
     move: Universal_set => [X H] P.
     exists {x in X | P x} => y.
     rewrite Specify_classification.
-    split; auto => [[H0 H1]] //.
+    firstorder.
   Qed.
 
 End Russell's_paradox.
