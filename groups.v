e971f82ab37683661a3e63ff501b74d989a53fc7
David Jao <djao@dominia.org>
2022-06-04 19:12:20 -0400

    Use implicit arguments for group axioms.

--- groups.v
+++ groups.v
@@ -5,7 +5,7 @@ Require Export iterated_ops integers.
 Record group :=
   mkGroup {
       Gset :> set;
-      id : elts Gset where "1" := id;
+      ident : elts Gset where "1" := ident;
       mul : elts Gset → elts Gset → elts Gset where "a * b" := (mul a b);
       inv : elts Gset → elts Gset where "/ a" := (inv a);
       assoc : ∀ a b c, a * (b * c) = (a * b) * c;
@@ -13,18 +13,24 @@ Record group :=
       mul_inv_r : ∀ a, a * / a = 1;
     }.
 
-Section Group_theorems.
+Arguments mul {g}.
+Arguments inv {g}.
+Arguments assoc {g}.
+Arguments mul_1_r {g}.
+Arguments mul_inv_r {g}.
 
-  Variable Group : group.
+Section Group.
+
+  Context {Group : group}.
   Notation G := (elts Group).
   Declare Scope Group_scope.
   Delimit Scope Group_scope with group.
   Open Scope Group_scope.
   Bind Scope Group_scope with G.
-  Notation "1" := (id Group) : Group_scope.
-  Infix "*" := (mul Group) : Group_scope.
-  Notation "/ a" := (inv Group a) : Group_scope.
-  Notation "/ 1" := (inv Group 1) : Group_scope.
+  Notation "1" := (@ident Group) : Group_scope.
+  Infix "*" := (@mul Group) : Group_scope.
+  Notation "/ a" := (inv a) : Group_scope.
+  Notation "/ 1" := (inv 1) : Group_scope.
 
   Definition IGS (g : G) := elt_to_set g : set.
 
@@ -42,16 +48,15 @@ Section Group_theorems.
   Theorem mul_1_l : ∀ a, 1 * a = a.
   Proof.
     move=> a.
-    rewrite -(mul_1_r _ (1 * a)) -assoc
-            -{2}(mul_inv_r _ (/a)) (assoc _ a) mul_inv_r assoc mul_1_r
-            -(mul_inv_r _ a) -assoc mul_inv_r mul_1_r //.
+    rewrite -{2}(mul_1_r a) -{2}(mul_inv_r (/a)) assoc mul_inv_r
+            -{2}(mul_1_r 1) -{3}(mul_inv_r a) -? assoc mul_inv_r mul_1_r //.
   Qed.
 
   Theorem mul_inv_l : ∀ a, / a * a = 1.
   Proof.
     move=> a.
-    rewrite -{2}(mul_1_r _ a) -{1}(mul_inv_r _ (/a)) (assoc _ a) mul_inv_r
-                                  mul_1_l mul_inv_r //.
+    rewrite -(mul_inv_r (/a)) -(mul_1_l (/ /a))
+            -(mul_inv_r a) -? assoc mul_inv_r mul_1_r //.
   Qed.
 
-End Group_theorems.
+End Group.
