d0f415f658f7365a0574a14a486d5f3c6daa9824
David Jao <djao@dominia.org>
2023-02-28 03:12:14 -0500

    improved proofs.

--- summation_example.v
+++ summation_example.v
@@ -3,14 +3,11 @@ Require Export rationals.
 
 Example Gauss : ∀ n, (2 * (sum ℤ (λ k, k : Z) 1 n) = n * (n + 1))%Z.
 Proof.
-  induction n using Induction.
+  induction n using Induction; rewrite ? sum_succ; auto using one_le_succ.
   - rewrite sum_neg -? [rings.zero ℤ]/0%Z -? [0%N : Z]/0%Z;
       eauto using naturals.succ_lt; ring.
-  - case: (classic (n = 0)%N) => [-> | /succ_0 [c H]].
-    + rewrite sum_0 -[S 0 : Z]/1%Z; ring.
-    + rewrite sum_succ /=; auto using one_le_succ.
-      rewrite integers.M1 integers.D1 -(integers.M1 2) IHn -add_1_r -? INZ_add
-              -[1%N : Z]/1%Z; ring.
+  - rewrite -add_1_r integers.M1 integers.D1 -(integers.M1 2) IHn -? INZ_add
+            -[1%N : Z]/1%Z; ring.
 Qed.
 
 Lemma IZQ_sum : ∀ (a b : N) (f : N → Z),
@@ -27,7 +24,7 @@ Qed.
 Example Gauss_Q : ∀ n, (sum ℚ (λ x, x : Q) 1 n) = n * (n + 1) / 2.
 Proof.
   move: (ordered_rings.zero_ne_2 ℤ_order) => /= /[dup] H /[swap] n.
-  rewrite inv_div // -IZQ_eq => H0.
+  rewrite inv_div // -IZQ_eq => {}H.
   apply (cancellation_mul_r (integral_domain_from_field ℚ) (2 : Q)) => // /=.
   by rewrite -M2 inv_l // -(M1 1) M3 M1 IZQ_sum IZQ_mul Gauss.
 Qed.
